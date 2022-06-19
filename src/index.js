57289fc61845a28eaed75f27902af52f7e04c615
ndikumanaisaie <ndikumanaisaie@gmail.com>
2022-06-19 23:11:21 +0100

    Add modal window to the comment and reservation buttons

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@
 import './styles/style.css';
 import './styles/bootstrap.min.css';
 import './styles/bootstrap-grid.min.css';
+import './bootstrap.bundle.min.js';
 
 const searchShow = async () => {
   const url = 'https://api.tvmaze.com/search/shows?q=girls';
   await fetch(url)
     .then((response) => response.json())
     .then((data) => {
-      // console.log(data);
-      // const result = data.map(element => element.show.url);
+      console.log(data);
       renderResult(data);
       document.querySelector('.errorMessage').innerHTML = '';
     })
@@ -22,6 +22,8 @@ const searchShow = async () => {
 function renderResult(results) {
   const resultList = document.querySelector('.shows');
   resultList.innerHTML = '';
+
+  // Creating navigation bar
   const navBar = `
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <a class="navbar-brand" href="#">TVShows logo</a>
@@ -43,12 +45,14 @@ function renderResult(results) {
   </div>
 </nav> `;
   resultList.insertAdjacentHTML('beforebegin', navBar);
+
+  // Inserting tv shows
   results.forEach((result) => {
     const element = document.createElement('div');
     element.classList.add('card');
     element.style.width = '20rem';
     element.innerHTML = `
-    <img src="${result.show.image.medium}" class="card-img-top" alt="Image of the show">
+    <img src="${result.show.image.original}" class="card-img-top w-100" alt="Image of the show">
     <div class="card-body">
       <div class="d-flex justify-content-between">
         <h5 class="card-title">${result.show.name}</h5>
@@ -59,8 +63,45 @@ function renderResult(results) {
       </div>
     </div>
     <div class="card-body">
-      <button type="button" class="btn btn-secondary">Comments</button>
-      <button type="button" class="btn btn-secondary">Reservations</button>
+      <!-- Button trigger modal -->
+      <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal${result.show.id}" >
+        Comments
+      </button>
+      <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal${result.show.id}" >
+        Reservations
+      </button>
+  
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal${result.show.id}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-xl modal-dialog-centered modal-dialog-scrollable">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              <img src="${result.show.image.original}" class="card-img-top" alt="Image of the show">
+              <div class="card-body">
+                <h5 class="card-title">${result.show.name}</h5>
+                <p class="card-text">${result.show.summary}</p>
+              </div>
+              <ul class="list-group list-group-flush">
+                <li class="list-group-item">Language: ${result.show.language}</li>
+                <li class="list-group-item">Run time: ${result.show.runtime}</li>
+                <li class="list-group-item">Premiered: ${result.show.premiered}</li>
+                <li class="list-group-item"><a href="${result.show.url}" class="card-link">Watch Here</a></li>
+              </ul>
+              <div class="card-body">
+                <a href="#" class="card-link">Comments</a>
+                <a href="#" class="card-link">Reservations</a>
+              </div>
+              </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            </div>
+        </div>
+        </div>
+    </div>
     </div>`;
     resultList.appendChild(element);
   });
